d236f8b6f081effffa4731764bfec6292dbb983c
Alexey Leshchenko <leshchenko@gmail.com>
2024-11-25 00:33:25 +0300

    Fix

--- sales_bot/bot/client.py
+++ sales_bot/bot/client.py
@@ -1,5 +1,5 @@
 from pyrogram import Client
 
-from ..config import API_HASH, API_ID, BOT_TOKEN
+from sales_bot.config import API_HASH, API_ID, BOT_TOKEN
 
 app = Client("sales_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
